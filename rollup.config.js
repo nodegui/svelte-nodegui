39f257dafbb99e09c8ea9bddb68bb830c208cb1e
halfnelson <dpershouse@gmail.com>
2019-02-25 23:51:10 +1000

    native transitions and rollup cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,8 @@ import pkg from './package.json'
 
 let externalModules = pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : []
 
+let localModules = ["dom", "svelte-helpers", "components", "transitions"]
+
 let plugins = [
   svelte({
     include: 'src/components/**/*.html',
@@ -17,40 +19,31 @@ let plugins = [
   resolve(),
 ]
 
-export default [{
-  input: 'src/index.ts',
-  output: [{
-    file: "dist/index.js",
-    format: 'cjs',
-  }],
-  external: (id) => [...externalModules, "./svelte-helpers", "./dom", "./components"].some(prefix => id.startsWith(prefix)),
-  plugins: plugins
-},
-{
-  input: 'src/dom/index.ts',
-  output: [{
-    file: "dist/dom/index.js",
-    format: 'cjs',
-  }],
-  external: (id) => [...externalModules, "../svelte-helpers/", "../components/"].some(prefix => id.startsWith(prefix)),
-  plugins: plugins
-},
-{
-  input: 'src/svelte-helpers/index.ts',
-  output: [{
-    file: "dist/svelte-helpers/index.js",
-    format: 'cjs',
-  }],
-  external: (id) => [...externalModules, "../dom", "../components"].some(prefix => id.startsWith(prefix)),
-  plugins: plugins
-},
-{
-  input: 'src/components/index.ts',
-  output: [{
-    file: "dist/components/index.js",
-    format: 'cjs',
-  }],
-  external: (id) => [...externalModules, "../dom", "../svelte-helpers"].some(prefix => id.startsWith(prefix)),
-  plugins: plugins
-},
+function module_defs() {
+  return localModules.map(mod => {
+    return {
+      input: `src/${mod}/index.ts`,
+      output: [{
+        file: `dist/${mod}/index.js`,
+        format: 'cjs',
+      }],
+      external: (id) => [...externalModules, ...localModules.filter(m => m != mod).map(m => `../${m}`)].some(prefix => id.startsWith(prefix)),
+      plugins: plugins
+    }
+  })
+
+}
+
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [{
+      file: "dist/index.js",
+      format: 'cjs',
+    }],
+    external: (id) => [...externalModules, ...localModules.map(m => `./${m}`)].some(prefix => id.startsWith(prefix)),
+    plugins: plugins
+  },
+  ...module_defs()
 ];
