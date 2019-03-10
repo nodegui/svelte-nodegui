882b1b1d73537bf2036b812e04b9972263f5a5a9
halfnelson <dpershouse@gmail.com>
2019-03-11 08:17:54 +1000

    try ts for tests

--- demo/app/tests/example.js
+++
@@ -1,7 +0,0 @@
-var svelte_native = require("svelte-native"); //Require the main view model to expose the functionality inside it.
-
-describe('svelte-native', function () {
-    it('exposes start function', function () {
-        assert.exists(svelte_native.svelteNative); //Assert that the counter equals 42.
-    });
-});
