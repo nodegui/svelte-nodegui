330003ecdbce335498446a58c4acdabe152f5617
halfnelson <dpershouse@gmail.com>
2019-03-16 08:29:40 +1000

    publish docs

--- docs/client/client.088b7932.js
+++
@@ -1,2 +0,0 @@
-import"./chunk.659cf27e.js";import{a as e}from"./chunk.2c927239.js";import"./chunk.abeba03e.js";e({target:document.querySelector("#sapper")});
-//# sourceMappingURL=client.088b7932.js.map
---
+++ docs/client/client.30d49ae8.js
@@ -0,0 +1,2 @@
+import"./chunk.659cf27e.js";import{a as e}from"./chunk.1e050625.js";import"./chunk.abeba03e.js";e({target:document.querySelector("#sapper")});
+//# sourceMappingURL=client.30d49ae8.js.map
