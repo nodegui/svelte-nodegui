44d8595428d149f0e4472661eb07693d80c3a8ec
halfnelson <dpershouse@gmail.com>
2019-03-25 08:04:47 +1000

    remove obsolete docs

--- docs/client/client.68c99b4d.js
+++
@@ -1,2 +0,0 @@
-import"./chunk.659cf27e.js";import{a as e}from"./chunk.cffe46bd.js";import"./chunk.abeba03e.js";e({target:document.querySelector("#sapper")});
-//# sourceMappingURL=client.68c99b4d.js.map
