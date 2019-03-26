1f3dbef8e34c8fc8e0a74697c6c1bf50efaa6a11
halfnelson <dpershouse@gmail.com>
2019-03-26 13:38:27 +1000

    all css is now global, to align with how svelte works

--- src/dom/svelte/HeadElement.ts
+++ src/dom/svelte/HeadElement.ts
@@ -1,6 +1,6 @@
 import { ElementNode, ViewNode } from "../basicdom";
 import StyleElement from "./StyleElement";
-import { topmost } from "tns-core-modules/ui/frame"
+import { addCss } from "tns-core-modules/application"
 
 export default class HeadElement extends ElementNode {
 
@@ -10,26 +10,9 @@ export default class HeadElement extends ElementNode {
 
     onInsertedChild(childNode: ViewNode, atIndex: number) {
         if (childNode instanceof StyleElement) {
-
-            let frame = topmost();
-
-            const applyStyleSheet = () => {
-                if (frame) {
-                    let css: string = (childNode as any).textContent;
-                    if (css) {
-                        console.log("adding frame css", css);
-                        frame.addCss(css);
-                        console.log("frame css is now", frame.css);
-                    }
-                }
-            }
-
-            if (!frame) {
-                console.log("no topframe, waiting for a tick")
-                //the rare occasion we have no top frame, usually a race at app start, we just wait for a tick
-                setTimeout(() => { frame = topmost(); applyStyleSheet() }, 0)
-            } else {
-                applyStyleSheet();
+            let css: string = (childNode as any).textContent;
+            if (css) {
+                addCss(css);
             }
         }
     }
