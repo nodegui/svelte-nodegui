acd4ac4d7ac31f2c19f052900b207c8b5dcb0f15
halfnelson <dpershouse@gmail.com>
2019-03-27 00:14:30 +1000

    handle text node updating

--- src/dom/basicdom/TextNode.ts
+++ src/dom/basicdom/TextNode.ts
@@ -13,4 +13,12 @@ export default class TextNode extends ViewNode {
         this.text = text
         this.parentNode.setText(text)
     }
+
+    set data(text: string) {
+        this.setText(text);
+    }
+
+    get data() {
+        return this.text;
+    }
 }
