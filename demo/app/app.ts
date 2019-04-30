4fcb5b41aca07c1b3921731c608e407300083b7c
halfnelson <dpershouse@gmail.com>
2019-04-30 22:14:46 +1000

    Enable trace for demo app

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -4,7 +4,11 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { svelteNative } from "svelte-native";
-import App from  "./App.svelte";
+import { svelteNative, DomTraceCategory } from "svelte-native";
+import * as trace from "tns-core-modules/trace"
+import App from "./App.svelte";
+trace.enable();
+trace.addCategories(DomTraceCategory);
 svelteNative(App, {});
+
 
