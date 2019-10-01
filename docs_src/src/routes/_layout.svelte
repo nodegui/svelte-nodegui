3c8daf5dabdd723ad25cab558b1d870dc4a1ebe1
halfnelson <dpershouse@gmail.com>
2019-10-02 08:49:19 +1000

    WIP update layout and index

--- docs_src/src/routes/_layout.svelte
+++ docs_src/src/routes/_layout.svelte
@@ -1,17 +1,42 @@
 <script>
 	
 	import { stores } from '@sapper/app';
-	import InlineSvg from '../components/InlineSvg.svelte';
-	import Nav from '../components/TopNav.svelte';
+	import { Icon, Icons, Nav, NavItem } from '@sveltejs/site-kit';
+	import PreloadingIndicator from '../components/PreloadingIndicator.svelte';
+
+/*	import InlineSvg from '../components/InlineSvg.svelte';
+	import Nav from '../components/TopNav.svelte'; */
 
 	export let segment;
 
 	const { page, preloading, session } = stores();
 </script>
 
-<InlineSvg />
+<Icons/>
+
+{#if $preloading}
+	<PreloadingIndicator/>
+{/if}
+
+
 {#if $page.path !== '/repl/embed'}
-	<Nav {segment}/>
+	<Nav {segment} {page} logo="logo.svg">
+		<NavItem segment="tutorial">Tutorial</NavItem>
+		<NavItem segment="docs">API</NavItem>
+		<NavItem segment="repl">REPL</NavItem>
+		<NavItem segment="blog">Blog</NavItem>
+		
+		<NavItem external="https://svelte.dev">Svelte</NavItem>
+		<NavItem external="https://docs.nativescript.org/start/introduction">NativeScript</NavItem>
+
+		<NavItem external="chat" title="Discord Chat">
+			<Icon name="message-square"/>
+		</NavItem>
+
+		<NavItem external="https://github.com/halfnelson/svelte-native" title="GitHub Repo">
+			<Icon name="github"/>
+		</NavItem>
+	</Nav>
 {/if}
 
 <main>
@@ -24,7 +49,7 @@
 	main {
 		position: relative;
 		margin: 0 auto;
-		padding: var(--nav-h) var(--side-nav) 0 var(--side-nav);
+		padding: var(--nav-h) 0 0 0;
 		overflow-x: hidden;
 	}
 </style>
