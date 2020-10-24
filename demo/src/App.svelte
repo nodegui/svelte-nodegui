39754649ab3ed3ca09440b4ce300a1bcc7343967
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-10-24 21:38:47 +0100

    feat: App.svelte more interesting

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,4 +1,26 @@
-<window bind:this={win}>
+<window
+    bind:this={win}
+    windowTitle="Hello World"
+    styleSheet={`
+        #myroot {
+            background-color: #009688;
+            height: '100%';
+            align-items: 'center';
+            justify-content: 'center';
+        }
+        #mylabel {
+            font-size: 16px;
+            font-weight: bold;
+            padding: 1;
+        }
+    `}
+>
+    <view nodeRole="centralWidget" id="myroot">
+        <flexLayout nodeRole="layout">
+            <text id="mylabel" text="Hello"></text>
+            <!-- <text style="color: red;">World</text> -->
+        </flexLayout>
+    </view>
     <!-- <text>Examples</text> -->
 </window>
 
@@ -8,12 +30,11 @@
     let win;
 
     onMount(() => {
-        console.log(`Mounted! win:`, win);
-        console.log(`Mounted! win.nativeView:`, win.nativeView);
+        // console.log(`Mounted! win:`, win);
+        // console.log(`Mounted! win.nativeView:`, win.nativeView);
         /**
          * @see https://github.com/nodegui/nodegui-starter/blob/master/src/index.ts#L44
          */
-        win.nativeView.setWindowTitle("Hello World");
         win.nativeView.show();
     })
 </script>
