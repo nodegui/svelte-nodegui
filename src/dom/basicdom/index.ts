13286c8925442bed6b2ae906ab756f3eab670d83
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-10-25 12:35:49 +0000

    chore: Remove previous DOM implementation (basicdom)

--- src/dom/basicdom/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as DocumentNode } from "./DocumentNode"
-export { default as ElementNode } from "./ElementNode"
-export { default as CommentNode } from "./CommentNode"
-export { default as PropertyNode } from "./PropertyNode"
-export { default as ViewNode, normalizeElementName } from "./ViewNode"
-export { default as TextNode } from "./TextNode"
-export { LogLevel, logger } from "./Logger"
-export { registerElement, createElement, RegisterElementOptions } from "./element-registry"
