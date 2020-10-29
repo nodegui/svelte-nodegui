f697a91fdb5b96a3ea89dc88231232a2584f4ba0
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-10-29 21:57:35 +0000

    chore: Adjust demo. `font-weight: 900;` works, but `font-weight: '900';` doesn't.

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -22,20 +22,18 @@
 </script>
 
 <window bind:this={win} windowTitle="Hello World">
-    <view id="container">
+    <view id="container" style="background-color: 'cyan';">
         <text text="The time is: {currentDate.toLocaleTimeString()}"></text>
-        <button class="nice_button" text="Press me"></button>
+        <button id="nice_button" text="Press me"></button>
     </view>
 </window>
 
 <style>
     #container {
         align-items: 'center';
-        justify-content: 'space-between';
-        background-color: 'cyan';
+        justify-content: 'space-around';
     }
-    .nice_button {
-        margin: 8px;
-        height: 30px;
+    #nice_button {
+        font-weight: 900;
     }
 </style>
