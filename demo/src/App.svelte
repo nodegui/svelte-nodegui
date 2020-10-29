167f1d0928fe786bc76afe54c623f5fab4562693
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-10-29 22:00:34 +0000

    chore: Clean up demo

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -2,18 +2,13 @@
     import { onMount } from 'svelte';
 
     let win;
-    let currentDate = new Date();
+    let date = new Date();
 
     onMount(() => {
-        /* `window` is an alias for `global`. We assign this to prevent garbage-collection of the main window. */
         window.win = win;
         win.nativeView.setWindowTitle("Hello World");
         win.nativeView.show();
-
-        let timer = setInterval(() => {
-            currentDate = new Date();
-        }, 1000);
-
+        let timer = setInterval(() => date = new Date(), 1000);
         return () => {
             clearInterval(timer);
             delete window.win;
@@ -23,7 +18,7 @@
 
 <window bind:this={win} windowTitle="Hello World">
     <view id="container" style="background-color: 'cyan';">
-        <text text="The time is: {currentDate.toLocaleTimeString()}"></text>
+        <text text="The time is: {date.toLocaleTimeString()}"></text>
         <button id="nice_button" text="Press me"></button>
     </view>
 </window>
