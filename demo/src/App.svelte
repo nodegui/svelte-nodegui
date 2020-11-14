fdfb83875ee123217c7bc0562aad4a82c8ea6bb6
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-11-14 14:32:20 +0000

    Accept "who" prop from app.ts

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import type { RNWindow } from "svelte-desktop/dom/react-nodegui/src/components/Window/RNWindow";
+
+    export let who: string = "uninitialised";
     let win;
     onMount(() => {
         (window as any).win = win; // Prevent garbage collection.
@@ -14,7 +16,7 @@
 
 <window bind:this={win} windowTitle="Hello World">
     <view id="container" style="background-color: 'yellow';">
-        <text>Some text with actual children</text>
+        <text>Hello {who}</text>
         <button on:clicked={(checked) => console.log("Clicked!", checked)} id="nice_button" text="Press me"/>
     </view>
 </window>
