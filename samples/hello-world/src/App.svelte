af627e8bdeee80108a48489ba9df148a3335e582
Saurav Sahu <mrsauravsahu@outlook.com>
2021-02-07 19:39:05 +0530

    move hello-world demo to samples folder

--- samples/hello-world/src/App.svelte
+++ samples/hello-world/src/App.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import open from "open";
     let win;
     onMount(() => {
-        (window as any).win = win; // Prevent garbage collection.
+        // Prevent garbage collection.
+        (window as any).win = win; 
         win.nativeView.show();
+
         return () => {
             delete (window as any).win;
         };
     });
+
+    const openDocs = () => {
+        open('https://google.com');
+    }
 </script>
 
 <svelte:options namespace="foreign" />
 <window bind:this={win} windowTitle="Hello World">
     <view id="container" style="background-color: '#41444A';">
-        <text style="color: white;">Some text with actual children</text>
-        <button on:clicked={(checked) => console.log("Clicked!", checked)} id="nice_button" text="Press me"/>
+        <text style="color: white;">Welcome to Svelte NodeGUI! 😎️</text>
+        <text>1. </text>
+        <button on:clicked={openDocs} id="nice_button" text="Press me"/>
     </view>
 </window>
 
@@ -22,6 +30,7 @@
     #container {
         align-items: 'center';
         justify-content: 'space-around';
+        padding: 8;
     }
     #nice_button {
         font-weight: 900;
